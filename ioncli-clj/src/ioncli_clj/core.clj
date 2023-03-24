f630c095c103ac3c0ee895b8367855e001e511ce
Abel Quiros <beluchin@gmail.com>
2023-03-24 19:26:03 +0100

    keywords as strings

--- ioncli-clj/src/ioncli_clj/core.clj
+++ ioncli-clj/src/ioncli_clj/core.clj
@@ -3,16 +3,41 @@
   (:gen-class)
   (:require ioncli-clj))
 
+(declare ->form)
 (defn -main [& args]
-  (let [fn-name (first args)
-        quoted-fn-args (map #(str "\"" % "\"") (rest args))]
-    (eval (read-string
-            (format "(ioncli-clj/%s %s)"
-                    fn-name
-                    (clojure.string/join " " quoted-fn-args))))
+  (let [form (->form args)]
+    (eval form)
 
     ;; the file monitoring service uses futures. Hence, we need to
     ;; terminate them explicitly - here instead of in the ensure-connect
     ;; function, because the function may run inside the REPL.
     ;; https://stackoverflow.com/a/27014732/614800
-    (shutdown-agents)))
+    (shutdown-agents))
+
+
+  #_(let [fn-name (first args)
+          quoted-fn-args (map #(str "\"" % "\"") (rest args))]
+      (eval (read-string
+              (format "(ioncli-clj/%s %s)"
+                      fn-name
+                      (clojure.string/join " " quoted-fn-args))))
+
+      ;; the file monitoring service uses futures. Hence, we need to
+      ;; terminate them explicitly - here instead of in the ensure-connect
+      ;; function, because the function may run inside the REPL.
+      ;; https://stackoverflow.com/a/27014732/614800
+      (shutdown-agents)))
+
+(declare kwds-as-strs)
+(defn- ->form [args]
+  (let [fn-form (read-string (str "ioncli-clj/" (first args)))
+        arg-forms (map #(-> %
+                            read-string
+                            ((fn [x] (cond
+                                       (symbol? x) %
+                                       (coll? x) (kwds-as-strs x)
+                                       :else x))))
+                       (rest args))]
+    (reverse (into `(~fn-form) arg-forms))))
+
+(defn- kwds-as-strs [coll] (map #(or (and (keyword? %) (name %)) %) coll))
